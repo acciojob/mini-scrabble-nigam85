c9f3dca286779baeb3a5bf1b89f2f24d4c859784
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-09-15 12:27:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your code here
+// Function to count letters and update the letter count in real-time
+function countLetters() {
+  // Get the input text from the textbox
+  const inputText = document.getElementById("evaluatedText").value;
+
+  // Count the number of characters (including spaces)
+  const letterCount = inputText.length;
+
+  // Update the letter count in the <h3> element
+  document.getElementById("letterCount").textContent = letterCount;
+}
+
+// Initially set the letter count to 0
+document.getElementById("letterCount").textContent = "0";
+
